ec51e41e0121fced211108ef4d77d1a2d92fdfa2
Manggala Pramuditya Wiryawan <manggala.wiryawan@gmail.com>
2025-02-18 12:03:38 +0700

    Brand and Category UI

---
+++ database/migrations/2025_02_12_081407_change_image_category_data__type.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            // Ubah tipe data kolom dari integer menjadi string
+            $table->string('image')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            // Kembalikan ke tipe data semula (misalnya integer)
+            $table->text('image')->change();
+        });
+    }
+};
